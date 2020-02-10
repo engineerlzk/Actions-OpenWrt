6c12f473da0d478c20ea6bfe9bb64da1e4072209
engineerlzk <engineerlzk@qq.com>
2020-02-10 11:48:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,5 +5,5 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
+# Modify default IP；修改缺省管理ip地址
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
